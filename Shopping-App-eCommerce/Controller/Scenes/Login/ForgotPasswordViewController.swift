b2514c3e7626fe84a1384846006d55bac08c7ee7
oemurlu <oemurlu@gmail.com>
2023-01-29 21:51:16 +0300

    add forgotViewController

--- Shopping-App-eCommerce/Controller/Scenes/Login/ForgotPasswordViewController.swift
+++ Shopping-App-eCommerce/Controller/Scenes/Login/ForgotPasswordViewController.swift
@@ -9,5 +9,31 @@ import UIKit
 import Firebase
 
 class ForgotPasswordViewController: UIViewController {
+
+    //MARK: - Properties
+    @IBOutlet weak var emailTextField: UITextField!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sendNewPasswordButtonPressed(_ sender: UIButton) {
+        if let email = emailTextField.text {
+            if email == "" {
+                DuplicateFuncs.alertMessage(title: "ERROR", message: "Please enter a valid email", vc: self)
+            } else {
+                Auth.auth().sendPasswordReset(withEmail: email) { error in
+                    if let error = error {
+                        DuplicateFuncs.alertMessage(title: "Network error", message: error.localizedDescription, vc: self)
+                    } else {
+                        DuplicateFuncs.alertMessageWithHandler(title: "Success", message: "Please check your email", vc: self) {
+                            self.performSegue(withIdentifier: K.forgotToLogin, sender: self)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
